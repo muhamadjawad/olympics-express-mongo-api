ec46ae0a24f0ffa52c98b7bd27d43ab8596f3eba
muhamadjawad <jawadmansoor1998@gmail.com>
2023-03-20 19:34:10 +0500

    on query

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -1,25 +1,32 @@
 const { eventsCollection } = require("../models/events")
 
 const getAllEvents = async (req, res) => {
+    console.log("O Event");
 
-    const { country, name, winner } = req.query;
-    const queryObject = {}
+    try {
+        const { country, name, winner } = req.query;
+        const queryObject = {}
+        console.log("req.query", req.query)
+        if (name) {
+            queryObject.name = { $regex: name, $options: "i" }//regex for search
+        }
 
-    if (name) {
-        queryObject.name = { $regex: name, $options: "i" }//regex for search
+        if (country) {
+            queryObject.country = { $regex: country, $options: "i" }//regex for search
+        }
+
+        if (winner) {
+            queryObject.winner = { $regex: winner, $options: "i" }
+        }
+
+        const allEvents = await eventsCollection.find(queryObject).sort("-date") // if sort is simple its ascending , if - then descending
+
+        res.status(200).json(allEvents)
+
+    } catch (error) {
+        console.log("error", error)
     }
 
-    if (country) {
-        queryObject.country = { $regex: country, $options: "i" }//regex for search
-    }
-
-    if (winner) {
-        queryObject.winner = { $regex: winner, $options: "i" }
-    }
-
-    const allEvents = await eventsCollection.find(queryObject)//name:"fif"
-
-    res.status(200).json(allEvents)
 }
 
 const createEvent = async (req, res) => {
