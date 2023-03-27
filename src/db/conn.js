96f3d8c5ce47ca4d9d143254617ad7ebbfadcfb7
muhamadjawad <jawadmansoor1998@gmail.com>
2023-03-27 16:00:14 +0500

    monggo db cloud conected

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,11 @@
+require("dotenv").config()
 const mongooose = require('mongoose')
 
-const uri = "mongodb://127.0.0.1:27017/olympics"
+// const uri = "mongodb://127.0.0.1:27017/olympics"
 
 
 const connectDB = () => {
-    mongooose.connect(uri, {
+    mongooose.connect(process.env.MONGO_DB_URL, {
     }).then(() =>
         console.log("connection  succesful")).
         catch((error) => {
