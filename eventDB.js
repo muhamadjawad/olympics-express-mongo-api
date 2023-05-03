296275580a09eb06d11087535f0cd909ba84852d
muhamadjawad <jawadmansoor1998@gmail.com>
2023-05-03 15:53:00 +0500

    unnecessary console removed

--- eventDB.js
+++ eventDB.js
@@ -1,14 +1,13 @@
 const { connectDB } = require("./src/db/conn")
-const {eventsCollection} = require("./src/models/events")
+const { eventsCollection } = require("./src/models/events")
 const EventJSON = require("./src/json/events.json")
 
 //by this you can save josn to collections
 const start = async () => {
     try {
         await connectDB()
-        console.log("eventsCollection", eventsCollection)
         await eventsCollection.insertMany(EventJSON)
-         await console.log("Successfully transfered data")
+        console.log("Successfully transfered data")
 
     } catch (error) {
         console.log(error);
